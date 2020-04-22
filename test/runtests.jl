d30263b4424587400c275e5ef79a58aaa845e587
Soulmer <95michael@wp.pl>
2020-04-22 21:14:53 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,8 @@
 using optmet, Test
 
-@test my_function(1) == 1.5
-@test my_function(2) == 1.5
+@testset "newton-raphson" begin
+	x, f = 1000.0, x -> x^4 -24x^3 +7x^2 - 11x + 420
+	@test minimum_nr(f, x) == (-32524.475393806228, 17.812172762347867)
+	x, f = 500.0, x -> x^4 - 7x^3 + 7x^2 - 77x +77
+	@test minimum_nr(f, x) == (-387.5919553775244, 5.277905093806142)
+end
